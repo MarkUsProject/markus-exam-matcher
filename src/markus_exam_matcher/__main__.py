5d16a250ac05596afb699119456cb2e85d593c98
donny-wong <141858744+donny-wong@users.noreply.github.com>
2025-07-04 10:02:12 -0400

    Adding QR code scanning feature (#11)

--- src/markus_exam_matcher/__main__.py
+++ src/markus_exam_matcher/__main__.py
@@ -11,6 +11,7 @@ from argparse import ArgumentParser
 
 from .core.char_types import CharType
 from .image_processing import read_chars
+from .qr_scan import scan_qr
 
 
 def config_arg_parser() -> ArgumentParser:
@@ -23,15 +24,18 @@ def config_arg_parser() -> ArgumentParser:
         prog="run_scanner.py", description="Predict handwritten characters in rectangular grids."
     )
 
+    # Scan type argument
+    parser.add_argument("scan_type", type=str, help="Scan type to be character or QR code")
+
     # Positional arguments
     parser.add_argument("image", type=str, help="Path to image to predict characters from.")
+
+    # Optional arguments
     parser.add_argument(
-        "char_type",
+        "--char_type",
         choices=["digit", "letter"],
         help="Type of character to classify. Only digits and letters are supported.",
     )
-
-    # Optional arguments
     parser.add_argument(
         "-d",
         "--debug",
@@ -46,8 +50,16 @@ if __name__ == "__main__":
     # Parse command line arguments
     parser = config_arg_parser()
     args = parser.parse_args(sys.argv[1:])
-    char_type = CharType.DIGIT if args.char_type == "digit" else CharType.LETTER
 
-    # Make prediction
-    pred = read_chars.run(args.image, char_type=char_type, debug=args.debug)
-    print(pred)
+    if args.scan_type == "char":
+        char_type = CharType.DIGIT if args.char_type == "digit" else CharType.LETTER
+
+        # Make prediction
+        pred = read_chars.run(args.image, char_type=char_type, debug=args.debug)
+        print(pred)
+    elif args.scan_type == "qr":
+        scanned_result = scan_qr.read_qr(args.image)
+        print(scanned_result)
+    else:
+        print("Unknown scan type.")
+        sys.exit(1)
